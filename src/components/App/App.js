9351d78ad0228bc495508c41364438e06d4f28f0
Ben Firth <bfirth93@gmail.com>
2020-07-09 10:01:03 -0600

    Add boilerplate compnents to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,23 +3,19 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {/* <Navbar /> */}
+      <section>
+        {/* <Intro /> */}
+        {/* <Projects /> */}
+        {/* <Skills /> */}
+        {/* <Experince /> */}
+        {/* <Education /> */}
+        {/* <Extra /> */}
+        {/* <Links /> */}
+      </section>
+    </main>
   );
-}
+};
 
 export default App;

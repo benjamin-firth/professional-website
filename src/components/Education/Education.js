128067c3edeefcb61ea2c402e7fab35bb6260214
Ben Firth <bfirth93@gmail.com>
2020-07-10 14:58:48 -0600

    Create education component boilerplate

---
+++ src/components/Education/Education.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section>
+      <h2>EDUCATION</h2>
+      <article>
+        <h3>Software Engineering Certification</h3>
+        <p>Turing School of Software and Design</p>
+        <p>At Turing I invested over 1600 hours across 8 months to become a job-ready software developer. In the Front-End Engineering program I built skills in HTML5, CSS, Design Fundamentals, core JavaScript, as well as React (class based and using Hooks) and Redux. In the final quarter, I built full stack applications utilizing Node and PostgreSQL.</p>
+      </article>
+      <article>
+        <h3>Bachelor's in Geography / GIS</h3>
+        <p>Western Washington University</p>
+      </article>
+    </section>
+  );
+};
+
+export default Education;

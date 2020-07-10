c0b9ffea4146b2f34374f22a619bc483051bce21
Ben Firth <bfirth93@gmail.com>
2020-07-10 15:07:31 -0600

    Create extra component boilerplate

---
+++ src/components/Extra/Extra.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Extra = () => {
+  return (
+    <section>
+      <h2>FUN EXTRA INFORMATION!</h2>
+      <ul>
+        <li>I'm an avid rock climber. You can follow my adventures HERE!</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Extra;

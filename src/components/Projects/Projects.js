4a08e248b03a2f7e35282874071e399294b197da
Ben Firth <bfirth93@gmail.com>
2020-07-16 14:54:56 -0600

    Add styled components to projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,23 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Projects = () => {
+const Projects = name => {
+  const createProj = name => {
+    return (
+      <StyledProject>
+        <h3>{name}</h3>
+      </StyledProject>
+    );
+  };
+
   return (
-    <section>
+    <StyledProjects>
       <h2>PROJECTS</h2>
-      <article>
-        <h3>Project One</h3>
-      </article>
-      <article>
-        <h3>Project Two</h3>
-      </article>
-      <article>
-        <h3>Project Three</h3>
-      </article>
-      <article>
-        <h3>Project Four</h3>
-      </article>
-    </section>
+      <StyledProjectContainer>
+        {createProj("Project One")}
+        {createProj("Project Two")}
+        {createProj("Project Three")}
+        {createProj("Project Four")}
+      </StyledProjectContainer>
+    </StyledProjects>
   );
 };
+
+const StyledProjects = styled.section`
+  h2 {
+    font-size: 1.8em;
+    margin: 70px;
+  }
+`;
+
+const StyledProjectContainer = styled.section`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const StyledProject = styled.article`
+  border: 1px solid;
+  cursor: pointer;
+  padding: 20px;
+  transition: all .2s ease-in-out;
+
+  &:hover {
+    opacity: 40%;
+    transform: scale(1.1);
+  }
+`;
+
+
 
 export default Projects;

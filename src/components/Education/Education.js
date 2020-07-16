be4b1881541190d75b0f707019799d323ddb261e
Ben Firth <bfirth93@gmail.com>
2020-07-16 15:55:30 -0600

    Add styled components to education component

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Education = () => {
   return (
-    <section>
+    <StyledEducation>
       <h2>EDUCATION</h2>
-      <article>
+      <StyledSchool>
         <h3>Software Engineering Certification</h3>
-        <p>Turing School of Software and Design</p>
-        <p>At Turing I invested over 1600 hours across 8 months to become a job-ready software developer. In the Front-End Engineering program I built skills in HTML5, CSS, Design Fundamentals, core JavaScript, as well as React (class based and using Hooks) and Redux. In the final quarter, I built full stack applications utilizing Node and PostgreSQL.</p>
-      </article>
-      <article>
+        <p className='school-title'>Turing School of Software and Design</p>
+        <p>At Turing I invested over 1600 hours across 8 months to become a job-ready software developer. In the Front-End Engineering program I built skills in HTML5, CSS, Design Fundamentals, core JavaScript, as well as React (class based and using Hooks) and Redux. In the final quarter, I built full stack applications utilizing Node, PostgreSQL, and GraphQL.</p>
+      </StyledSchool>
+      <StyledSchool>
         <h3>Bachelor's in Geography / GIS</h3>
-        <p>Western Washington University</p>
-      </article>
-    </section>
+        <p className='school-title'>Western Washington University</p>
+      </StyledSchool>
+    </StyledEducation>
   );
 };
+
+const StyledEducation = styled.section`
+  padding: 70px;
+
+  h2 {
+    font-size: 1.8em;
+    margin-bottom: 70px;
+  }
+`;
+
+const StyledSchool = styled.article`
+  margin-bottom: 50px;
+
+  p {
+    font-size: 1.1em;
+  }
+
+  .school-title {
+    font-style: italic;
+  }
+`;
 
 export default Education;

6cddce0406b6ba1d278f4193c27f026dceaee597
Ben Firth <bfirth93@gmail.com>
2020-10-01 15:01:40 -0600

    Add instagram feed styling

--- src/components/Extra/Extra.js
+++ src/components/Extra/Extra.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactTinyLink } from 'react-tiny-link';
+import { useInstagramFeed } from 'use-instagram-feed';
 
 const Extra = () => {
+  let photos = useInstagramFeed({
+    userId: "5647382267",
+    thumbnailWidth: 640,
+    photoCount: 4,
+  });
+
   return (
     <StyledExtra id="extra">
-      <h2 data-aos="zoom-in">FUN EXTRA INFORMATION!</h2>
+      <h2 data-aos="fade-up-right">FUN EXTRA INFORMATION!</h2>
       <ul>
         <li>If you like listening to me ramble about software, you can check out my blog posts <a 
           target="_blank"
@@ -13,19 +19,26 @@ const Extra = () => {
           rel="noopener noreferrer"  
           href="https://medium.com/@bfirth93"><strong>here.
         </strong></a></li>
-        <li>I'm an avid rock climber. You can follow my adventures <a 
+        <li>I'm an avid rock climber, mountain biker, and outdoor enthusiast. You can follow my adventures on Instagram <a 
           target="_blank"
           aria-label="Link to my climbing adventures"
           rel="noopener noreferrer"  
           href="https://www.instagram.com/truckbedboulderers/?hl=en"><strong>HERE!
         </strong></a></li>
-        <ReactTinyLink
-            cardSize="small"
-            showGraphic={true}
-            maxLine={2}
-            minLine={1}
-            url="https://www.instagram.com/p/CEZ3U3SpS2a/"
-        />
+        <StyleImageContainer>
+          {photos &&
+            photos.map(({ id, caption, src, width, height, url }) => (
+              <a 
+                key={id} 
+                href={url} 
+                className="insta-image" 
+                rel="noopener noreferrer" 
+                target="_blank">
+                <img src={src} alt={caption} width='200px' />
+              </a>
+            ))
+          }
+        </StyleImageContainer>
       </ul>
     </StyledExtra>
   );
@@ -61,6 +74,28 @@ const StyledExtra = styled.section`
 
     h2 {
       text-align: center;
+    }
+  }
+`;
+
+const StyleImageContainer = styled.div`
+  display: flex;
+  height: 200px;
+  margin-top: 40px;
+
+  .insta-image {
+    margin-right: 20px;
+
+    img {
+      border: 3px solid #f3bda5;
+      border-radius: 5px;
+      transition: all .2s ease-in-out;
+    }
+  }
+
+  .insta-image:hover {
+    img {
+      width: 210px;
     }
   }
 `;
